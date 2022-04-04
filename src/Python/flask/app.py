ab335d83cb6c0a911d1a6d41eef72f0ba270f9a2
mwaseem75 <mwaseem75@hotmail.com>
2022-04-04 06:38:42 +0300

    Update app.py

--- src/Python/flask/app.py
+++ src/Python/flask/app.py
@@ -70,4 +70,6 @@ def graphdb2():
     return render_template('index.html', nodes = got_net.nodes,edges=got_net.edges)    
 
 if __name__ == '__main__':
-      app.run('0.0.0.0', port = "8080", debug=True)
+      #app.run('0.0.0.0', port = "8080", debug=True)
+      app.run('irisglobalgraphdb.demo.community.intersystems.com', port = "8090", debug=True)
+        
