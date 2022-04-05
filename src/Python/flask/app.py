245977f1071d0082212c106821649e4ecf4fb97c
mwaseem75 <mwaseem75@hotmail.com>
2022-04-05 05:50:21 +0300

    commit

--- src/Python/flask/app.py
+++ src/Python/flask/app.py
@@ -2,20 +2,21 @@ from flask import Flask, render_template, request
 from pyvis.network import Network
 from irisglobal import IRISGLOBAL
 
-
 app = Flask(__name__) 
 app.secret_key = "**2a*2d2*"
 
 #Mian route. (index)
 @app.route("/")
 def index():
+    #Establish connection and import data to globals
     irisglobal = IRISGLOBAL()
     irisglobal.import_g1_nodes_edges()
     irisglobal.import_g2_nodes_edges()
-    #getting nodes data
+    #getting nodes data from globals
     nodes = irisglobal.get_g1nodes()
-    #getting edges data
+    #getting edges data from globals
     edges = irisglobal.get_g1edges()
+    #To display graph with configuration
     pyvis = True
     return render_template('index.html', nodes = nodes,edges=edges,pyvis=pyvis)    
 
@@ -23,6 +24,7 @@ def index():
 @app.route("/dynamic", methods=('GET', 'POST'))
 def dynamic():
     if request.method == 'POST':
+       #Generate dynamic graph based on number of nodes entered by user 
        val = request.form['nodes']
        irisglobal = IRISGLOBAL()
        irisglobal.import_dynamic_nodes_edges(val)
@@ -31,17 +33,18 @@ def dynamic():
        #getting edges data
        edges = irisglobal.get_g3edges()
     else:
+       #Initially show empty graph 
        nodes = ''
        edges = ''
     
     return render_template('dynamic.html', nodes = nodes,edges=edges)    
 
 @app.route("/graphdb2")
+#Demonstrate Network Science by using Network Game of Thrones dataset
 def graphdb2():
     irisglobal = IRISGLOBAL()
-
+    #use pyvis.network functionality to create nodes and edges
     got_net = Network()
-
     # set the physics layout of the network
     got_net.barnes_hut()
     got_data = irisglobal.get_g2data()
